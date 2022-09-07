7484bcae1b86e34b0906146f058355307eeeec94
Andrew Shen <azshen23@gmail.com>
2022-09-07 16:42:16 -0400

    Update testUsersApi.test.ts

--- tests/testUsersApi.test.ts
+++ tests/testUsersApi.test.ts
@@ -1,11 +1,21 @@
-const request = require("supertest")("http:/localhost:3001");
+const request = require("supertest")("https://api1.nuocal.com");
 const expect = require("chai").expect;
+require("dotenv").config();
 
 describe("GET /getUser", function () {
   it("returns an object of the request user", async function () {
-    const response = await request.get("/getUser?input=1");
+    const response = await request
+      .get("/getUser?input=1")
+      .set("Authorization", "bearer " + process.env.AUTH_TOKEN);
 
     expect(response.status).to.eql(200);
-    expect(response.body.data.length).to.eql(30);
+  });
+
+  it("returns a list of posts from the user has created", async function () {
+    const response = await request
+      .get("/getUserPosts?input=1")
+      .set("Authorization", "bearer " + process.env.AUTH_TOKEN);
+
+    expect(response.status).to.eql(200);
   });
 });
